b7f9c76ae9d6c07d30a0bcdd7c5669dc0be2cd4a
mohan772 <pmohan2104@gmail.com>
2026-06-18 14:40:40 +0530

    Fix Express 5 SPA fallback for Render

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Serve frontend
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
